6418618e39554928d013071016ca079ce0370704
David Zbarsky <dzbarsky@gmail.com>
2020-12-02 16:16:22 -0500

    Add http package

---
+++ http/handler.go
@@ -0,0 +1,31 @@
+package http
+
+import (
+	"io"
+	"net/http"
+	"strings"
+
+	polyfill "js-polyfill-server"
+)
+
+func Handler() func(w http.ResponseWriter, r *http.Request) {
+	c := polyfill.New()
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		out, err := c.PolyfillsByName(r.UserAgent(),
+			strings.Split(r.Form.Get("features"), ","))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		_, err = io.Copy(w, out)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
